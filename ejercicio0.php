2e3f8ba222f372559a93a7ad67cdbba128875b1b
jozzekc <30834008+jozzekc@users.noreply.github.com>
2017-08-10 18:40:06 +0200

    Add files via upload

---
+++ ejercicio0.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ejercicio 0</title>
+
+
+    <link href="style.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+  </head>
+  <body>
+
+
+<div class="php">
+
+    <?php
+    $num="33";
+
+      echo "<h2>El Numero $num </h2>";
+    echo "---------------------------------------";
+
+if ($num>0) { echo  "<h4>Es  Un numero positivo </h4>";  }
+elseif ($num<0){  echo "<h4>Es Un numero negativo </h4>";  }
+
+?>
+<a href="index.html">Atras</a>
+
+    <script src="js/bootstrap.min.js"></script>
+      <script src="js/jquery.min.js"></script>
+</div>
+  </body>
+</html>
